aee133140bf388d0f707b9277f3ab6f5e8139c90
Aobo Guo <aobo.guo@gmail.com>
2015-08-07 15:53:07 +0200

    Made more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,15 @@ Portion.destroy_all
 
 User.create! username: "kleog", password: "password", password_confirmation: "password", phone_number: "284-123-4920", email: "email1@email.com"
 User.create! username: "aobog", password: "password", password_confirmation: "password", phone_number: "846-753-0924", email: "email2@email.com"
+User.create! username: "carol_from_once", password: "password", password_confirmation: "password", phone_number: "846-753-0924", email: "carol@carol.com"
 puts "Creating users..."
 
 
-Listing.create! product: "eggs", required_amount: 20, bulk_cost: 100, owner: User.first.username
-Listing.create! product: "chips", required_amount: 10, bulk_cost: 80, owner: User.second.username
+Listing.create! product: "eggs", required_amount: 30, bulk_cost: 85, owner: User.first.username
+Listing.create! product: "papayas", required_amount: 3, bulk_cost: 30, owner: User.second.username
+Listing.create! product: "toilet paper", required_amount: 18, bulk_cost: 105, owner: User.first.username
+Listing.create! product: "nappies", required_amount: 108, bulk_cost: 260, owner: User.first.username
+Listing.create! product: "soap", required_amount: 4, bulk_cost: 30, owner: User.second.username
 puts "Creating listings..."
 
 
@@ -25,4 +29,8 @@ Portion.create! listing_id: Listing.first.id, user_id: User.first.id, share: 5
 Portion.create! listing_id: Listing.second.id, user_id: User.first.id, share: 2
 Portion.create! listing_id: Listing.first.id, user_id: User.second.id, share: 3
 Portion.create! listing_id: Listing.second.id, user_id: User.second.id, share: 6
+Portion.create! listing_id: Listing.third.id, user_id: User.second.id, share: 10
+Portion.create! listing_id: Listing.fourth.id, user_id: User.first.id, share: 24
+Portion.create! listing_id: Listing.fourth.id, user_id: User.second.id, share: 60
+Portion.create! listing_id: Listing.fifth.id, user_id: User.first.id, share: 2
 puts "Creating portions..."
